0bb37744f2c19cfa40e6d1bd05475eb050570080
jwantit <kgw072089@gmail.com>
2026-03-25 23:11:35 +0900

    [level 2] Title: 가장 큰 수, Time: 1325.51 ms, Memory: 26.9 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.py
@@ -1,6 +1,6 @@
 def solution(numbers):
     arr = list(map(str, numbers))
-     
-    arr.sort(key=lambda x : x*3 ,reverse=True)
+    arr = sorted(arr, key=lambda x: x*3, reverse=True)
     
     return str(int(''.join(arr)))
+    
